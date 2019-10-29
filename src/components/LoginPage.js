1b05b378767153931a816437918804fdd82fef2f
djkajo <carlosserraamorim@gmail.com>
2019-10-29 23:58:40 +0000

    loginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,13 @@ import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({startLogin}) => (
 
-    <div>    
-          <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+        <div className = "box-layout__box">
+        <h1 className= "box-layout__title"> Despesismo </h1>
+        <p> Mantenha as suas despesas sob controlo </p>
+        <button onClick={startLogin}>Entrar</button>
+        </div>
+        
     </div>
 
 );
